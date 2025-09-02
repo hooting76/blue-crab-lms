3436f7c42fc1528dd3a3ff777ee6026d30aaa50b
jch <ggongco1120@gmail.com>
2025-09-03 01:21:30 +0900

    로그인후 메인컴포넌트

--- component/auth/UserDashboard.jsx
+++ component/auth/UserDashboard.jsx
@@ -7,18 +7,7 @@ function UserDashboard() {
   return (
     <div className="">
       <div className="">
-        <div className="">{user.avatar}</div>
         <h2 className="">환영합니다!</h2>
-        
-        {/* 사용자 정보 */}
-        <div className="">
-          <p className="">
-            <strong>이름:</strong> {user.name}
-          </p>
-          <p className="">
-            <strong>이메일:</strong> {user.email}
-          </p>
-        </div>
         
         {/* 사용자 메뉴 */}
         <div className="">
