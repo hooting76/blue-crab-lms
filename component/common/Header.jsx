d2f6fc597b62359b14a3490427ba993058d93b19
jch <ggongco1120@gmail.com>
2025-09-03 19:04:03 +0900

    데이터 호출 및 퍼블리싱 작업

--- component/common/Header.jsx
+++ component/common/Header.jsx
@@ -1,55 +1,67 @@
 import React from 'react';
 import { UseUser } from '../../hook/UseUser';
+import HeaderCss from '../../css/modules/Header.module.css';
 
 function Header() {
   const { user, isAuthenticated, logout } = UseUser();
 
+  const Reset = () => {
+    window.location.replace('/');
+  }
+
   return (
     <header>
-      <div>
-        <div>
+        <h1 onClick={Reset}>
+          <picture className={HeaderCss.logoImg}>
+            <img src='./favicon/android-icon-72x72.png' alt='Logo'/>
+          </picture>
+          Blue-Crab LMS
+        </h1>
+
           {/* 로그인된 사용자 정보 */}
           {isAuthenticated && (
             <div>
               <div>
-                <div>
-                  <span>아이콘</span>
-                  
+              {/* user info init */}
+
                   {/* user_idx */}
                   {user && user.data.user && (
                     <input type='hidden' id='idx' value={JSON.stringify(user.data.user.id)} />
                   )}
 
-                  <div>
-                      {/* user_name*/}
+                  {/* user_name*/}
+                  <div className={HeaderCss.userInfo}>
                       {user && user.data.user && (
-                        <p>{JSON.stringify(user.data.user.name)}</p>
-                      )}
-
-                      {/* user_email */}
-                      {user && user.data.user && (
-                        <p>{JSON.stringify(user.data.user.email)}</p>
+                        <p>
+                          <span className={HeaderCss.userName}>
+                            {JSON.stringify(user.data.user.name).replace('"','').replace('"','')}
+                          </span>
+                          {user && user.data.user && (
+                            <>{JSON.stringify(user.data.user.student) == 1 && ('교수')}</>
+                          )}
+                          님 안녕하세요
+                        </p>
                       )}
                   </div>
-                </div>
               </div>
+              {/* user info end */}
+            
+
               
-              {/* 간단한 로그아웃 버튼 */}
-              <button onClick={logout} title="로그아웃" >
+
+              <button 
+                onClick={logout} 
+                title="로그아웃" 
+                className={HeaderCss.logoutBtn}
+              >
                 로그아웃
               </button>
+
             </div>
           )}
           
           {/* 로그인되지 않은 상태 */}
-          {!isAuthenticated && (
-            <div>
-              로그인이 필요합니다
-            </div>
-          )}
-          
-        </div>
-      </div>
+          {!isAuthenticated && ( null )}
     </header>
   );
 }
