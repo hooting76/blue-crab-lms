2949a361230967b194afff14d7a8f56d0bcb9c05
jch <ggongco1120@gmail.com>
2025-09-04 17:27:37 +0900

    로그인 이후 메인페이지 기본골격 작업

--- component/auth/UserDashboard.jsx
+++ component/auth/UserDashboard.jsx
@@ -1,23 +1,52 @@
 import React from 'react';
 import { UseUser } from '../../hook/UseUser';
+import CalendarComp from '../common/CalendarComp';
+
+import UserDashCss from '../../css/modules/UserDashboard.module.css';
 
 function UserDashboard() {
   const { user, logout } = UseUser();
 
   return (
-    <div className="">
-      <div className="">
-        <h2 className="">환영합니다!</h2>
-        
-        {/* 사용자 메뉴 */}
-        <div className="">
-          <button
-            onClick={logout}
-            className="">로그아웃
-          </button>
+    <>
+      <main className={UserDashCss.main}>
+        {/* notice wrap */}
+        <section className={UserDashCss.notice}>
+          <div className={UserDashCss.noticeFirst}>
+            <h4>공지사항</h4>
+            <ul>
+              <li>1. asdf</li>
+              <li>2. qwer</li>
+            </ul>
+          </div>
+          <div className={UserDashCss.noticeSecond}>
+            <h4>취업정보</h4>
+            <ul>
+              <li>1. asdf</li>
+              <li>2. qwer</li>
+            </ul>
+          </div>
+        </section>
+        {/* notice wrap end*/}
+
+        {/* calendar wrap */}
+        <section className={UserDashCss.calendar}>
+          <CalendarComp />
+        </section>
+        {/* calendar wrap end*/}
+      </main>
+      <aside className={UserDashCss.aside}>
+        <div className={UserDashCss.userInfo}>
+          asdf
         </div>
-      </div>
-    </div>
+        <div className={UserDashCss.service}>
+          qwer
+        </div>
+        <div className={UserDashCss.quickMenu}>
+          zxcv
+        </div>
+      </aside>    
+    </>
   );
 }
 
