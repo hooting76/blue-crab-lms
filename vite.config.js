9fa0d3738ae2cd5a9038b8e0dfa484c87188d60d
jch <ggongco1120@gmail.com>
2025-09-04 17:22:56 +0900

    npm build 관련 빌드 용량 한계치 조정

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
 
+    build: {
+      chunkSizeWarningLimit: 5000, // kb 단위
+    },
+
     define: {
       global: {},
     },
