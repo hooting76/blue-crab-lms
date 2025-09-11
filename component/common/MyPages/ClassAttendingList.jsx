62d07dc20b89a533b1004681e4211e82eeb56ffd
NDH <ry77@naver.com>
2025-09-11 16:37:03 +0900

    '수강중인 과목' 페이지 1차

--- component/common/MyPages/ClassAttendingList.jsx
+++ component/common/MyPages/ClassAttendingList.jsx
@@ -1,8 +1,39 @@
+import '../../../css/MyPages/ClassAttendingList.css'
+import classAttendingDummy from '../../../src/mock/classAttendingDummy.js'; //임시로 만든 더미데이터.
+
 function ClassAttendingList() {
     return(
-        <>
-            수강과목 목록
-        </>
+        <div className="classAttending_list_container">
+            <table className="classAttending_list_header">
+                <thead>
+                    <tr>
+                        <th style={{ width: "5%" }}>순번</th>
+                        <th style={{ width: "10%" }}>종별</th>
+                        <th style={{ width: "25%" }}>교과목명</th>
+                        <th style={{ width: "20%" }}>강의시간</th>
+                        <th style={{ width: "15%" }}>수업계획</th>
+                        <th style={{ width: "15%" }}>담당교수</th>
+                        <th style={{ width: "10%" }}>학점</th>
+                    </tr>
+                </thead>
+            </table>
+
+            <table className="classAttending_list_body">
+                <tbody>
+                    {classAttendingDummy.map((cls, index) => (
+                    <tr key={cls.LEC_IDX}>
+                        <td style={{ width: "5%" }}>{index + 1}</td>
+                        <td style={{ width: "10%" }}>{cls.LEC_BASIC == 1 ? "전공" : "교양"}</td>
+                        <td style={{ width: "25%" }}>{cls.LEC_NAME}</td>
+                        <td style={{ width: "20%" }}>{cls.LEC_TIME}</td>
+                        <td style={{ width: "15%" }}></td>
+                        <td style={{ width: "15%" }}>{cls.LEC_PROF}</td>
+                        <td style={{ width: "10%" }}>{cls.LEC_POINT}</td>
+                    </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
