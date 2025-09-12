d0d90385bac377c9a2a9631072f70b19f553c2ea
NDH <ry77@naver.com>
2025-09-12 16:43:27 +0900

    이미지 경로 수정

--- component/common/Introductions/Organization.jsx
+++ component/common/Introductions/Organization.jsx
@@ -1,9 +1,10 @@
-import "../../../css/Introductions/Organization.css"
+import "../../../css/Introductions/Organization.css";
+import '../../../src/assets/AjouPharmOrganization.jpeg';
 
 function Organization() {
     return(
         <div className="blueCrabOrganization">
-            <img src="../../../../src/assets/AjouPharmOrganization.jpeg" height={"500px"}/>
+            <img src="../../../src/assets/AjouPharmOrganization.jpeg" height={"500px"}/>
         </div>
     )
 }
