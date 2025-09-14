cb95ff9201114bb004afe7d0fc8efec688ba8c3c
jch <ggongco1120@gmail.com>
2025-09-15 00:17:09 +0900

    이메일로 인증코드 보내기 기능

---
+++ component/admin/auth/RequestCode.jsx
@@ -0,0 +1,60 @@
+
+async function RequestCode(token) {
+    let sessionToken = token
+    const baseUrl = 'https://bluecrab.chickenkiller.com/BlueCrab-1.0.0';
+
+    if(!sessionToken){
+        console.error('❌ sessionToken이 없습니다.');
+        return;
+    }
+
+    if (!sessionToken.startsWith('Bearer ')) {
+        sessionToken = 'Bearer ' + sessionToken;
+    }
+
+    // email code request
+    try {
+        const response = await fetch(`${baseUrl}/api/admin/email-auth/request`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': sessionToken
+            }            
+        });
+
+        const result = await response.json();
+        
+        if (response.status === 500) {
+            console.log("Server Error!!");
+            console.log('에러 메시지:', result.message);
+        } else if (result.success) {
+            console.log('✅ 2단계 성공! 이메일 확인 후 T.verify() 실행');
+        }
+
+        alert(result.message.replace("%d", result.data));
+        //timer
+        const timerElement = document.getElementById('authTimer');
+        let timeLeft = (result.data) * 60; // seconds
+
+        const countdownTimer = setInterval(() => {
+            const minutes = Math.floor(timeLeft / 60);
+            const seconds = timeLeft % 60;
+            timerElement.textContent = `${minutes}:${seconds.toString().padStart(2, '0')}`;
+            
+            if (timeLeft <= 0) {
+                clearInterval(countdownTimer);
+                timerElement.textContent = '0:00';
+                alert("인증코드가 만료되었습니다.\n 새로 인증을 진행해주세요.");
+                location.reload();
+            }
+            timeLeft--;
+        }, 1000);
+        
+        return result;
+        
+    } catch (error) {
+        console.error('❌ 네트워크 오류:', error);
+    }
+};
+
+export default RequestCode;
