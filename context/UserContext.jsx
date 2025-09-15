a2f84fb795854ad0152817ddd1db3e0bda0dde9f
NDH <ry77@naver.com>
2025-09-15 13:30:47 +0900

    로그아웃 함수에 메인페이지 이동기능 추가... 안되면 바로 롤백할게요

--- context/UserContext.jsx
+++ context/UserContext.jsx
@@ -150,6 +150,7 @@ export function UserProvider({ children }) {
     localStorage.removeItem('user');
     sessionStorage.removeItem('user');
     // console.log('🏁 Redis 통합 로그아웃 테스트 완료');
+    window.location.replace('/');
   };
 
   // 에러 클리어 함수
