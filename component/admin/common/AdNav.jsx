3ce3508a14c47ffd292a210de4b0acf052570d99
NDH <ry77@naver.com>
2025-09-24 14:31:10 +0900

    adNav 수정

--- component/admin/common/AdNav.jsx
+++ component/admin/common/AdNav.jsx
@@ -1,9 +1,7 @@
 import AdNavCss from "../../../css/admin/module/AdNav.module.css";
-import { useState } from 'react';
 
 // custom hook
 function AdNav({currentPage, setCurrentPage}){
-    const [currentPage, setCurrentPage] = useState("");
     return(
         <nav className={AdNavCss.nav}>
             <div onClick={setCurrentPage("학사공지")}>공지</div>
