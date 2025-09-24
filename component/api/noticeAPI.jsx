81b940e7c79bffc8912d22264a6e23ebf2f65753
NDH <ry77@naver.com>
2025-09-24 17:36:51 +0900

    acessToken 추가

--- component/api/noticeAPI.jsx
+++ component/api/noticeAPI.jsx
@@ -1,5 +1,10 @@
 async function getNotices() {
-  const response = await fetch('https://bluecrab.chickenkiller.com/Bluecrab-1.0.0/api/boards/list');
+  const response = await fetch('https://bluecrab.chickenkiller.com/Bluecrab-1.0.0/api/boards/list', {
+    headers : {
+      'Authorization' : `Bearer ${acessToken}`,
+      'Content-Type' : 'application/json'
+    }
+  }); 
   if (!response.ok) {
     throw new Error('Failed to fetch notices');
   }
