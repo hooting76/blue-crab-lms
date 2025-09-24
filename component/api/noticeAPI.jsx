a340322ed3fd143c584025864d64f63d13f09ef3
NDH <ry77@naver.com>
2025-09-24 13:02:41 +0900

    게시글 불러오기 POST->GET으로 수정

--- component/api/noticeAPI.jsx
+++ component/api/noticeAPI.jsx
@@ -1,18 +1,18 @@
 export async function getNotices(BOARD_CODE, page, size) {
-    const response = await fetch("https://bluecrab.chickenkiller.com/Bluecrab-1.0.0/api/boards/list", {
-        method: "POST",
+    const queryParams = new URLSearchParams({
+        BOARD_CODE,
+        page,
+        size
+    });
+
+    const response = await fetch(`https://bluecrab.chickenkiller.com/Bluecrab-1.0.0/api/boards/list?${queryParams.toString()}`, {
+        method: "GET",
         headers: {
             "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            BOARD_CODE,
-            page,
-            size
-        })
+        }
     });
 
     if (!response.ok) {
-        // HTTP 에러 처리
         throw new Error(`HTTP error! status: ${response.status}`);
     }
 
