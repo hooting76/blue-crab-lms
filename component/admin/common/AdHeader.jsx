1c1c5ca322e62af6df4829a95160871a2130c54e
jch <ggongco1120@gmail.com>
2025-10-06 18:08:42 +0900

    어드민 메뉴 기초 분류 및 로그아웃 버튼 추가

--- component/admin/common/AdHeader.jsx
+++ component/admin/common/AdHeader.jsx
@@ -1,12 +1,15 @@
 import AdHeaderCss from '../../../css/admin/module/AdHeader.module.css';
+import { UseAdmin } from '../../../hook/UseAdmin';
 
 function AdHeader(){
-
+    const { AdLogout } = UseAdmin();
     return(
         <header className={AdHeaderCss.header}>
-            header
+            <button onClick={AdLogout}>
+                로그아웃
+            </button>
         </header>
     );
-}
+}// AdHeader end
 
 export default AdHeader;
