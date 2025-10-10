a11875b5735feb98a904e27a011ae39c6b3d8d26
NDH <ry77@naver.com>
2025-10-10 11:32:46 +0900

    재시도

--- component/common/Communities/AdminNoticeWritingPage.jsx
+++ component/common/Communities/AdminNoticeWritingPage.jsx
@@ -93,8 +93,6 @@ function AdminNoticeWritingPage({ notice, accessToken: propToken, currentPage, s
   };
 
 console.log('업로드할 파일 목록:', selectedFiles);
-console.log('게시글 번호(boardIdx):', boardIdx);
-
 
 
   // 공지 작성
