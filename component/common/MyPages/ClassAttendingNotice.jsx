259eff2e5dad3d49e06857923bc33c23afd02621
NDH <ry77@naver.com>
2025-10-14 13:03:08 +0900

    수강과목 공지사항 페이지 조금 수정

--- component/common/MyPages/ClassAttendingNotice.jsx
+++ component/common/MyPages/ClassAttendingNotice.jsx
@@ -1,7 +1,28 @@
-function ClassAttendingNotice() {
+import React, { useState } from 'react';
+import { UseUser } from '../../../hook/UseUser';
+import ProfNoticeWritingPage from './ProfNoticeWritingPage.jsx';
+
+function ClassAttendingNotice({currentPage, setCurrentPage}) {
+    const {user} = UseUser();
+
+    const profNoticeWrite = () => {
+    setCurrentPage("과목별 공지 작성");
+    }
+
+    if (currentPage === "과목별 공지 작성") {
+        return (
+            <ProfNoticeWritingPage currentPage={currentPage} setCurrentPage={setCurrentPage}/>
+        );
+    }
+
     return(
         <>
             수강과목 공지사항
+
+            <div className="profNoticeWriteBtnArea">
+                {user.data.user.userStudent === 1 &&
+                <button className="profNoticeWriteBtn" onClick={profNoticeWrite}>과목별 공지 작성</button>}
+            </div>
         </>
     )
 }
