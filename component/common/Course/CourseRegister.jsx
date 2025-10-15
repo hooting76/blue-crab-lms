a0bc58d31c98d91c4868574db1f0f2a30163a949
NDH <ry77@naver.com>
2025-10-15 13:32:17 +0900

    강의 등록 페이지 중간저장

--- component/common/Course/CourseRegister.jsx
+++ component/common/Course/CourseRegister.jsx
@@ -8,65 +8,99 @@ function CourseRegister() {
     const [maxStudents, setMaxStudents] = useState(0);
     const [credit, setCredit] = useState(0);
 
+    const BASE_URL = 'https://bluecrab.chickenkiller.com/BlueCrab-1.0.0/api';
+
+    const submitCourseRegister = async (e) => {
+        const CourseRegisterDetails = {
+            lectureName,
+            lectureCode,
+            lectureDescription,
+            maxStudents,
+            credit
+        }
+
+        try {
+            const response = await fetch(`${BASE_URL}/lectures`, {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${accessToken}`
+                },
+                body: JSON.stringify(CourseRegisterDetails),
+            });
+
+        if (!response.ok) throw new Error('서버 에러가 발생했습니다.');
+        } catch (error) {
+        alert(error.message);
+        }
+    }
+
     return (
-        <form>
-            <div>
-                <label>강의 제목</label><br/>
-                <input
-                type="text"
-                value={lectureName}
-                onChange={(e) => setLectureName(e.target.value)}
-                required
-                style={{ width: '100%', padding: '8px', marginBottom: '16px' }}
-                />
-            </div>
+        <>
+            <form>
+                <div>
+                    <label>강의 제목</label><br/>
+                    <input
+                    type="text"
+                    value={lectureName}
+                    onChange={(e) => setLectureName(e.target.value)}
+                    required
+                    style={{ width: '100%', padding: '8px', marginBottom: '16px' }}
+                    />
+                </div>
 
-            <div>
-                <label>강의 코드</label><br/>
-                <input
-                type="text"
-                value={lectureCode}
-                onChange={(e) => setLectureCode(e.target.value)}
-                required
-                style={{ width: '100%', padding: '8px', marginBottom: '16px' }}
-                />
-            </div>
+                <div>
+                    <label>강의 코드</label><br/>
+                    <input
+                    type="text"
+                    value={lectureCode}
+                    onChange={(e) => setLectureCode(e.target.value)}
+                    required
+                    style={{ width: '100%', padding: '8px', marginBottom: '16px' }}
+                    />
+                </div>
 
-            <div>
-                <label>강의 설명</label><br/>
-                <input
-                type="text"
-                value={lectureDescription}
-                onChange={(e) => setLectureDescription(e.target.value)}
-                required
-                style={{ width: '100%', padding: '8px', marginBottom: '16px' }}
-                />
-            </div>
+                <div>
+                    <label>강의 설명</label><br/>
+                    <input
+                    type="text"
+                    value={lectureDescription}
+                    onChange={(e) => setLectureDescription(e.target.value)}
+                    required
+                    style={{ width: '100%', padding: '8px', marginBottom: '16px' }}
+                    />
+                </div>
 
-            <div>
-                <label>수강 인원</label><br/>
-                <input
-                type="number"
-                value={maxStudents}
-                onChange={(e) => setMaxStudents(e.target.value)}
-                required
-                style={{ width: '100%', padding: '8px', marginBottom: '16px' }}
-                />
-            </div>
+                <div>
+                    <label>수강 인원</label><br/>
+                    <input
+                    type="number"
+                    value={maxStudents}
+                    onChange={(e) => setMaxStudents(e.target.value)}
+                    required
+                    style={{ width: '100%', padding: '8px', marginBottom: '16px' }}
+                    />
+                </div>
 
-            <div>
-                <label>학점</label><br/>
-                <input
-                type="number"
-                value={credit}
-                onChange={(e) => setCredit(e.target.value)}
-                required
-                style={{ width: '100%', padding: '8px', marginBottom: '16px' }}
-                />
-            </div>
+                <div>
+                    <label>학점</label><br/>
+                    <input
+                    type="number"
+                    value={credit}
+                    onChange={(e) => setCredit(e.target.value)}
+                    required
+                    style={{ width: '100%', padding: '8px', marginBottom: '16px' }}
+                    />
+                </div>
+
+                
+            </form>
 
             
-        </form>
+            <div className='courseRegisterBtn'>
+                <button onClick={submitCourseRegister}>강의 등록</button>
+            </div>
+        </>
     )
 }
 
