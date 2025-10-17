b124b85ff45c4cce5a1117efdf8dae2b85a73675
NDH <ry77@naver.com>
2025-10-17 15:08:33 +0900

    강의 수정(교수 전용) 페이지도 수정

--- component/common/MyPages/ClassAttending.jsx
+++ component/common/MyPages/ClassAttending.jsx
@@ -14,17 +14,6 @@ function ClassAttending({currentPage, setCurrentPage}) {
 
     console.log("user : ", user);
 
-    // const [openRow, setOpenRow] = useState(null);
-    
-    // const totalCredits = classAttendingDummy.reduce(
-    //     (sum, cls) => sum + (Number(cls.LEC_POINT) || 0),
-    //     0
-    // ); // 해당 학기 총 이수 학점
-
-    // const handleRowClick = (index) => {
-    //     setOpenRow(openRow === index ? null : index);
-    // };
-
     // select 변경 핸들러
     const handleSemesterChange = (e) => {
         setSelectedSemester(e.target.value);
@@ -225,67 +214,7 @@ const fetchLectureList = async (accessToken, selectedSemester) => {
                     )}
                 </div>
             </div>
-        {/* <div className='classAttending_text'>
-            수강과목 클릭 시 해당과목 진행사항 표시
-        </div>
 
-        <div className="table-wrap">
-            <table>
-                <thead className='classAttending_list_header'>
-                    <tr>
-                        <th style={{ width: "5%" }}>순번</th>
-                        <th style={{ width: "10%" }}>종별</th>
-                        <th style={{ width: "25%" }}>교과목명</th>
-                        <th style={{ width: "20%" }}>강의시간</th>
-                        <th style={{ width: "15%" }}>수업계획</th>
-                        <th style={{ width: "15%" }}>담당교수</th>
-                        <th style={{ width: "10%" }}>학점</th>
-                    </tr>
-                </thead>
-                <tbody className='classAttending_list_body'>
-                    {classAttendingDummy.map((cls, index) => (
-                        <React.Fragment key={cls.LEC_IDX}>
-                            <tr
-                                className="clickable-row"
-                                onClick={() => handleRowClick(index)}
-                                style={{ cursor: 'pointer' }}
-                            >
-                                <td>{index + 1}</td>
-                                <td>{cls.LEC_BASIC === "1" ? "전공" : "교양"}</td>
-                                <td>{cls.LEC_NAME}</td>
-                                <td>{cls.LEC_TIME}</td>
-                                <td></td>
-                                <td>{cls.LEC_PROF}</td>
-                                <td>{cls.LEC_POINT}</td>
-                            </tr>
-                            {openRow === index && (
-                                <tr className="class_details_row">
-                                    <td colSpan={7}>
-                                        <div className="class-details">
-                                            <strong>{cls.LEC_NAME}</strong> 과목의 {selectedSemester === currentSemesterValue ? "진행사항" : "점수"}<br/><br/>
-                                            출석수 : <br/>
-                                            중간고사 점수 : <br/>
-                                            기말고사 점수 : <br/>
-                                            과제 1 점수 : <br/>
-                                            과제 2 점수 : <br/>
-                                            {selectedSemester !== currentSemesterValue && (<><br/>총합 점수 및 등급 :</>)}
-                                            {/* 지나간 학기에 들었던 강의는 총점 및 등급 표시 */}
-                                        {/* </div>
-                                    </td>
-                                </tr>
-                            )}
-                        </React.Fragment>
-                    ))}
-                </tbody>
-            </table>
-            </div>
-
-
-            <div className='totalpoints'>
-                이수학점: {totalCredits}점
-                {selectedSemester !== currentSemesterValue && "학기 평점: "} */}
-                {/* 지나간 학기는 총 평점 표시 */}
-            {/* </div> */}
         </div>
     );
 }
--- component/common/MyPages/CourseList.jsx
+++ component/common/MyPages/CourseList.jsx
@@ -6,7 +6,7 @@ import CourseDetailEdit from './CourseDetailEdit';
 function CourseList({ currentPage, setCurrentPage }) {
 
     const { user } = UseUser();
-    const [courseList, setCourseList] = useState([]);
+    const [lectureList, setLectureList] = useState([]);
     const [selectedLecture, setSelectedLecture] = useState(null);
     const [fetchedLecture, setFetchedLecture] = useState(null);
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -27,29 +27,82 @@ function CourseList({ currentPage, setCurrentPage }) {
     console.log("user : ", user);
     console.log("accessToken : ", accessToken);
 
-    const getCourseList = async (accessToken) => {
-        try {
-            const response = await fetch(`${BASE_URL}/professor/lectures`, {
-                method: 'POST',
-                headers: {
-                'Authorization': `Bearer ${accessToken}`,
-                'Content-Type': 'application/json'
-                },
-            body: JSON.stringify({})
-            });
-            if (!response.ok) throw new Error('강의 목록을 불러오는 데 실패했습니다.');
-            const data = await response.json();
-            setCourseList(data); // ✅ 받아온 데이터 저장
-        } catch (error) {
-            console.error('강의 목록 조회 에러:', error);
+    
+            // select 변경 핸들러
+            const handleSemesterChange = (e) => {
+                setSelectedSemester(e.target.value);
+            };
+        
+        const today = new Date();
+        let currentYear = today.getFullYear();
+        const currentMonth = today.getMonth() + 1;
+        
+        let currentSemester;
+        
+        if (currentMonth >= 3 && currentMonth <= 8) {
+            currentSemester = 1;
+        } else if (currentMonth >= 9) {
+            currentSemester = 2;
+        } else {
+            // 1~2월은 전년도 2학기
+            currentYear -= 1;
+            currentSemester = 2;
         }
-    };
+        
+        
+        // 현재 학기를 기준으로 지난 8개 학기 생성
+        const generateSemesters = (count = 8) => {
+            const semesters = [];
+            let year = currentYear;
+            let semester = currentSemester;
+        
+            for (let i = 0; i < count; i++) {
+                const value = `${year}_${semester}`;
+                const label = `${year}년 ${semester}학기`;
+                semesters.push({ value, label });
+        
+                // 이전 학기로 이동
+                if (semester === 1) {
+                    semester = 2;
+                    year -= 1;
+                } else {
+                    semester = 1;
+                }
+            }
+        
+            return semesters;
+        };
+        
+        const semesterOptions = generateSemesters(8);
+        const currentSemesterValue = `${currentYear}_${currentSemester}`; // 현재 학기 value
+        const [selectedSemester, setSelectedSemester] = useState(currentSemesterValue); // 학기 선택 상태
 
-    useEffect(() => {
-        if (accessToken) {
-            getCourseList(accessToken);
-        }
-    }, [accessToken]); // ✅ accessToken이 생겼을 때 호출
+
+    const fetchLectureList = async (accessToken, selectedSemester) => {
+        try {
+            const [year, semester] = selectedSemester.split('_');
+    
+            const response = await fetch(`${BASE_URL}/lectures`, {
+                method: "POST",
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({page: 0, size: 20, year: parseInt(year), semester: parseInt(semester)})
+            });
+        if (!response.ok) throw new Error('강의 목록을 불러오는 데 실패했습니다.');
+                const data = await response.json();
+                setLectureList(data); // ✅ 받아온 데이터 저장
+            } catch (error) {
+                console.error('강의 목록 조회 에러:', error);
+            }
+        };
+    
+        useEffect(() => {
+                fetchLectureList(accessToken, selectedSemester);
+            }, [accessToken, selectedSemester]); // ✅ accessToken이 생겼을 때, 학기가 선택되었을 때 호출
+    
+            console.log("lectureList : ", lectureList);
 
 
 
@@ -78,16 +131,24 @@ function CourseList({ currentPage, setCurrentPage }) {
 
     return (
         <>
-        <div>
-            <h2>강의 목록</h2>
-            <ul>
-                {courseList.map((course) => (
-                    <li key={course.lectureIdx} onClick={() => openModal(course)}>{course.lectureName}</li>
+            <select value={selectedSemester} onChange={handleSemesterChange} className='selectSemester'>
+                {semesterOptions.map((option) => (
+                    <option key={option.value} value={option.value}>
+                        {option.label}
+                    </option>
                 ))}
-            </ul>
-        </div>
+            </select>
 
-        {isModalOpen && (
+            <div>
+                <h2>강의 목록</h2>
+                <ul>
+                    {lectureList.map((course) => (
+                        <li key={course.lectureIdx} onClick={() => openModal(course)}>{course.lectureName}</li>
+                    ))}
+                </ul>
+            </div>
+
+            {isModalOpen && (
                 <div className="modal-overlay" onClick={closeModal}>
                     <div
                         className="modal-content"
