8df46090b522684db16790e6f0e4c41d64963744
NDH <ry77@naver.com>
2025-10-20 12:20:41 +0900

    재수정

--- component/common/MyPages/ClassAttendingNotice.jsx
+++ component/common/MyPages/ClassAttendingNotice.jsx
@@ -59,7 +59,7 @@ function ClassAttendingNotice({ rows, currentPage, setCurrentPage }) {
 
             if (!response.ok) throw new Error('공지사항 조회 실패');
             const data = await response.json();
-            setNoticeList(data);
+            setNoticeList(data.Array);
         } catch (error) {
             console.error('공지사항 에러:', error);
             setNoticeList([]);
