e08a3e638f05675636790805980ca9a21750f406
NDH <ry77@naver.com>
2025-10-23 12:58:45 +0900

    3차 시도

--- component/common/MyPages/ClassAttending.jsx
+++ component/common/MyPages/ClassAttending.jsx
@@ -104,7 +104,6 @@ const fetchNotices = async () => {
             if (!response.ok) throw new Error('공지사항 조회 실패');
             const data = await response.json();
             setNoticeList(data.content);
-            setTotalNotices(data.totalElements);
         } catch (error) {
             console.error('공지사항 에러:', error);
             setNoticeList([]);
