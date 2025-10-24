f9c082c59229a0f5020fe622e065d9ea948c1dcf
NDH <ry77@naver.com>
2025-10-24 14:48:25 +0900

    학생 강의 목록 조회 재시도

--- component/common/MyPages/ClassAttending.jsx
+++ component/common/MyPages/ClassAttending.jsx
@@ -235,17 +235,34 @@ const fetchNotices = async () => {
   return (
     <div className="classAttending_list_container">
       {/* 강의 선택 박스 */}
-      <select className="lectureName" value={selectedLectureSerial || ''} onChange={handleLectureChange}>
+      <select
+        className="lectureName"
+        value={selectedLectureSerial || ''}
+        onChange={handleLectureChange}
+      >
         {lectureList.length > 0 ? (
+          isProf ? (
+            // 교수일 때: lectureList 그대로 보여주기
             lectureList.map((lec) => (
-            <option key={lec.lecSerial} value={lec.lecSerial}>
+              <option key={lec.lecSerial} value={lec.lecSerial}>
                 {lec.lecTit}
-            </option>
+              </option>
             ))
+          ) : (
+            // 학생일 때: lectureList.content에서 정보 추출
+            lectureList.map((lec) =>
+              lec.content.map((item, idx) => (
+                <option key={`${lec.lecSerial}-${idx}`} value={item.serial}>
+                  {item.title}
+                </option>
+              ))
+            )
+          )
         ) : (
-            <option disabled>강의 목록 없음</option>
+          <option disabled>강의 목록 없음</option>
         )}
       </select>
+
 
 
       <div className="classAttendingContent">
