d8efa56fb762b7357f15094db131965b31f3248e
NDH <ry77@naver.com>
2025-10-30 10:04:33 +0900

    순위 백분율->점수 백분율

--- component/common/MyPages/MyScoreModal.jsx
+++ component/common/MyPages/MyScoreModal.jsx
@@ -81,7 +81,7 @@ console.log("scoreDetail : ", scoreDetail);
                         <div>총점<br/>
                             <span>점수 : {scoreDetail.total.score}</span>
                             <span>만점 : {scoreDetail.total.maxScore}</span>
-                            <span>순위 백분율 : {scoreDetail.total.percentage}</span>
+                            <span>점수 백분율 : {scoreDetail.total.percentage}</span>
                         </div>
                     </>
                 ) : (
